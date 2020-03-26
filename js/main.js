8afe4677bdce672832fd40af7d1f8a475b837fcc
Blakejfox <blakefox@foxteck.com>
2020-03-26 17:37:31 -0500

    Sigin Button via handburger dropdown the  now works. Adding better descriptions for all of the pricing packages. Adding getting started you tube video

--- js/main.js
+++ js/main.js
@@ -296,15 +296,17 @@ jQuery(document).ready(function($) {
   var OnePageNavigation = function() {
     var navToggler = $('.site-menu-toggle');
    	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
-      e.preventDefault();
 
       var hash = this.hash;
-
-      $('html, body').animate({
-        'scrollTop': $(hash).offset().top
-      }, 800, 'easeInOutCirc', function(){
-        window.location.hash = hash;
-      });
+	  if($(hash).offset().top!= undefined)
+	  {
+		e.preventDefault();
+		$('html, body').animate({
+			'scrollTop': $(hash).offset().top
+		}, 800, 'easeInOutCirc', function(){
+			window.location.hash = hash;
+		});
+	  }
 
     });
   };
